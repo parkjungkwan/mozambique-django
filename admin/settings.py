023111e7dd0d00f2e43369b96582257bd735168b
parkjungkwan <pakjkwan@gmail.com>
2022-11-29 16:52:37 +0900

    16

--- admin/settings.py
+++ admin/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "hello.apps.HelloConfig",
+    # "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Hello(models.Model):
+    use_in_migrations = True
+    hello = models.CharField(primary_key=True, max_length=30)
+    name = models.TextField()
+
+    class Meta:
+        db_table = "hellos"
+
+    def __str__(self):
+        return f'{self.pk} {self.name}'
